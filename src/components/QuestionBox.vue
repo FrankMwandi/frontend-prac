66608f8d8c1125eca4870d7663c9d87ff0b94cfa
Frank Mwandi <frankmwandi93@gmail.com>
2020-05-08 05:55:01 -0900

    initial commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,32 @@
+<template>
+       <div class="question-box-container">
+  <b-jumbotron>
+    
+
+    <template v-slot:lead>
+     {{currentQuestion.question}}
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+      List of answers
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#">Next Question</b-button>
+  </b-jumbotron>
+</div>
+       
+</template>
+
+<script>
+export default {
+    props: {
+        currentQuestion:object
+    }
+}
+
+
+
+</script>
